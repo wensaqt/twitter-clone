0f843875c9ecfaf4e2397bc584b9d3d35c4b4aea
faycalEdderi <faycmx@live.fr>
2025-03-10 15:29:39 +0100

    homepage refactoring

--- components/auth/index.tsx
+++ components/auth/index.tsx
@@ -1,84 +1,79 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import React, { useCallback } from 'react'
-import Button from '../ui/button'
-import { FcGoogle } from 'react-icons/fc'
-import { AiFillGithub } from 'react-icons/ai'
-import useRegisterModal from '@/hooks/useRegisterModal'
-import RegisterModal from '../modals/register-modal'
-import useLoginModal from '@/hooks/useLoginModal'
-import LoginModal from '../modals/login-modal'
-import { signIn } from 'next-auth/react'
+import Image from "next/image";
+import React, { useCallback } from "react";
+import Button from "../ui/button";
+import useRegisterModal from "@/hooks/useRegisterModal";
+import RegisterModal from "../modals/register-modal";
+import useLoginModal from "@/hooks/useLoginModal";
+import LoginModal from "../modals/login-modal";
 
 export default function Auth() {
-	const registerModal = useRegisterModal()
-	const loginModal = useLoginModal()
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
 
-	const onOpenRegisterModal = useCallback(() => {
-		registerModal.onOpen()
-	}, [registerModal])
+  const onOpenRegisterModal = useCallback(() => {
+    registerModal.onOpen();
+  }, [registerModal]);
 
-	const onOpenLoginModal = useCallback(() => {
-		loginModal.onOpen()
-	}, [loginModal])
+  const onOpenLoginModal = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
 
-	return (
-		<>
-			<RegisterModal />
-			<LoginModal />
-			<div className='grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen'>
-				<Image src={'/images/y.svg'} alt='X' width={450} height={450} className='justify-self-center order-2 hidden md:block border-2 rounded-full border-white' />
+  return (
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen">
+        <Image
+          src={"/images/y.svg"}
+          alt="X"
+          width={450}
+          height={450}
+          className="justify-self-center order-2 hidden md:block border-2 rounded-full border-white"
+        />
 
-				<div className='flex flex-col justify-center md:justify-between gap-6 order-1 h-full md:h-[70vh]'>
-					<div className='block md:hidden'>
-						<Image src={'/images/y.svg'} alt='X' width={50} height={50} />
-					</div>
-					<h1 className='text-6xl font-bold'>C'est maintenant !</h1>
-					<div className='w-full md:w-[60%]'>
-						<h2 className='font-bold text-3xl mb-4'>Rejoignez aujourd'hui.</h2>
-						<div className='flex flex-col space-y-2'>
-							<Button
-								onClick={() => signIn('google')}
-								label={
-									<div className='flex gap-2 items-center justify-center'>
-										<FcGoogle />
-										S'inscrire avec google
-									</div>
-								}
-								fullWidth
-								secondary
-							/>
-							<Button
-								onClick={() => signIn('github')}
-								label={
-									<div className='flex gap-2 items-center justify-center'>
-										<AiFillGithub />
-										S'inscrire avec Github
-									</div>
-								}
-								fullWidth
-								secondary
-							/>
-							<div className='flex items-center justify-center'>
-								<div className='h-px bg-gray-700 w-1/2' />
-								<p className='mx-4'>ou</p>
-								<div className='h-px bg-gray-700 w-1/2' />
-							</div>
-							<Button label={'Créer un compte'} fullWidth onClick={onOpenRegisterModal} />
-							<div className='text-[10px] text-gray-400'>
-								En vous inscrivant, vous acceptez les <span className='text-orange-500'>Termes d'utilisation</span> et
-								<span className='text-orange-500'> Politique de vie privée</span>, incluant l'utilisation de
-								<span className='text-orange-500'> Cookies</span>.
-							</div>
-						</div>
-					</div>
-					<div className='w-full md:w-[60%]'>
-						<h3 className='font-medium text-xl mb-4'>Déjà inscrit ?</h3>
-						<Button label={"Se connecter"} fullWidth outline onClick={onOpenLoginModal} />
-					</div>
-				</div>
-			</div>
-		</>
-	)
+        <div className="flex flex-col justify-center md:justify-between gap-6 order-1 h-full md:h-[50vh]">
+          <div className="block md:hidden">
+            <Image src={"/images/y.svg"} alt="X" width={50} height={50} />
+          </div>
+          <h1 className="text-6xl font-bold">C'est maintenant !</h1>
+          <div className="w-full md:w-[60%]">
+            <h2 className="font-bold text-3xl mb-4">Rejoignez aujourd'hui.</h2>
+            <div className="flex flex-col space-y-2">
+              <Button
+                label={"Créer un compte"}
+                fullWidth
+                onClick={onOpenRegisterModal}
+              />
+              <div className="text-[10px] text-gray-400">
+                En vous inscrivant, vous acceptez les{" "}
+                <span className="text-orange-500">Termes d'utilisation</span> et
+                <span className="text-orange-500">
+                  {" "}
+                  Politique de vie privée
+                </span>
+                , incluant l'utilisation de
+                <span className="text-orange-500"> Cookies</span>.
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="h-px bg-gray-700 w-1/2" />
+                <p className="mx-4">ou</p>
+                <div className="h-px bg-gray-700 w-1/2" />
+              </div>
+              <div className="w-full md:w-[60%]">
+                <h3 className="font-medium text-xl mb-4">Déjà inscrit ?</h3>
+                <Button
+                  label={"Se connecter"}
+                  fullWidth
+                  outline
+                  onClick={onOpenLoginModal}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
